1367e5075acdc3dddf0cb1cdeaa19b3a6132eed0
Mahabubul Hasan <mahabubul470@gmail.com>
2025-12-24 12:37:23 +0600

    fix: Update version endpoint URL to match repository name

--- supervisor/const.py
+++ supervisor/const.py
@@ -19,7 +19,7 @@ SUPERVISOR_DOCKER_NAME: str = f"{DOCKER_PREFIX}_supervisor"
 URL_HASSIO_ADDONS = "https://github.com/home-assistant/addons"
 URL_HASSIO_APPARMOR = "https://version.home-assistant.io/apparmor_{channel}.txt"
 # URL_HASSIO_VERSION = "https://version.home-assistant.io/{channel}.json"
-URL_HASSIO_VERSION = "https://my-smart-homes.github.io/version-data/data.json"
+URL_HASSIO_VERSION = "https://my-smart-homes.github.io/version/data.json"
 
 SUPERVISOR_DATA = Path("/data")
 
